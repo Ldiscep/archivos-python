4378e7e03569d6314d8d3c49980e47305d21d7e8
Ldiscep <lucianodiscepolo@gmail.com>
2024-04-05 11:07:56 -0300

    agregado enumerate.py y actualizado index.py clase 5/4/24

---
+++ Strings/enumerate.py
@@ -0,0 +1,6 @@
+#enumerate sirve para devolver una tupla con el numero de posicion de indice y el elemento del objeto iterable
+
+ejemplo="hola me llamo Luciano"
+ejemplo=enumerate(ejemplo)
+for a in ejemplo:
+    print(a)
