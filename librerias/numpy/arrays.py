946c105260b321abf2e3038f3642806078d25245
Ldiscep <lucianodiscepolo@gmail.com>
2024-04-11 13:32:26 -0300

    qwerty

--- librerias/numpy/arrays.py
+++ librerias/numpy/arrays.py
@@ -4,9 +4,6 @@ array1=np.array([1,2,3,4,5,6,7,8,9,10])
 array2 = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0])
 print(f"array1: {array1}")
 
-#preguntar a santi por un 
-#AttributeError: partially initialized module 'numpy' has no attribute 'array' (most likely due to a circular import)
-
 #operaciones elementales entre arrays:
 v1=np.array([1,2,3,4,5])
 v2=np.array([6,7,8,9,10])
