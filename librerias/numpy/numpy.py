a9660320b8956e854ecb93dcbd1a7f97f27069b5
Ldiscep <lucianodiscepolo@gmail.com>
2024-04-11 12:42:39 -0300

    numpy

---
+++ librerias/numpy/numpy.py
@@ -0,0 +1,8 @@
+#en un vector de numpy van todos datos del mismo tipo (int/float)
+import numpy  as np
+array1=np.array([1,2,3,4,5,6,7,8,9,10])
+array2 = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0])
+print(f"array1: {array1}")
+
+#preguntar a santi por un 
+#AttributeError: partially initialized module 'numpy' has no attribute 'array' (most likely due to a circular import)
