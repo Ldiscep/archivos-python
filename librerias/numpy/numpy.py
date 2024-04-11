6d5da2d38ed8b37fc17c1924138a34fb3682ec02
Ldiscep <lucianodiscepolo@gmail.com>
2024-04-11 12:51:19 -0300

    numpy

--- librerias/numpy/numpy.py
+++ librerias/numpy/numpy.py
@@ -6,3 +6,17 @@ print(f"array1: {array1}")
 
 #preguntar a santi por un 
 #AttributeError: partially initialized module 'numpy' has no attribute 'array' (most likely due to a circular import)
+
+"""
+uint8: int de 8 bits, puede representar numeros [0, 255].
+caso de overflow: 250 + 10 = 4 (cero cuenta)
+int8: int de 8 bits, puede representar numeros [-128, 127]
+
+uint16: int de 16 bits, puede representar numeros [0, 65535]
+int16: int de 16 bits, puede representar numeros [-32768, 32767]
+
+uint32: int de 32 bits, puede representar numeros [0, 4294967295]
+int32: int de 32 bits, puede representar numeros [-2147483648, 2147483647]
+
+y asi...
+"""
